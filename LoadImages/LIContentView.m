ce7aa42af6b1e5ebe1a17ca2d798efe6da9f0df3
Tong Guo <Tong-G@outlook.com>
2014-07-22 13:44:51 +0800

    Routine backup.

--- LoadImages/LIContentView.m
+++ LoadImages/LIContentView.m
@@ -43,78 +43,90 @@
 
 - ( void ) drawRect: ( NSRect )_Rect
     {
-#if 0   // Concurrent drawing
-    dispatch_queue_t defaultGlobalQueue = dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 );
-    dispatch_queue_t serialQueue = dispatch_queue_create( "individual.TongG.fuckQueue", DISPATCH_QUEUE_CONCURRENT );
-
-    srand( ( unsigned )time( NULL ) );
-
-    NSMutableArray* rectArray = [ NSMutableArray arrayWithCapacity: 10000 ];
-    NSSize currentSize = [ self bounds ].size;
-    for ( int index = 0; index < 100000; index++ )
-        {
-        CGFloat randomWidth = ( CGFloat )( rand() % ( NSInteger )currentSize.width );
-        CGFloat randomHeight = ( CGFloat )( rand() % ( NSInteger )currentSize.height );
-
-        CGFloat randomX = ( CGFloat )( rand() %20 + 10 );
-        CGFloat randomY = ( CGFloat )( rand() %20 + 10 );
-
-        [ rectArray addObject: [ NSValue valueWithRect: NSMakeRect( randomWidth, randomHeight, randomX, randomY ) ] ];
-        }
-
-    NSArray* colorArray = @[ [ NSColor grayColor ],  [ NSColor blackColor ]
-                           , [ NSColor cyanColor ],   [ NSColor greenColor ]
-                           , [ NSColor purpleColor ], [ NSColor orangeColor ]
-                           , [ NSColor darkGrayColor ], [ NSColor yellowColor ]
-                           ];
-
-    dispatch_async( serialQueue
+    dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 )
                   , ^( void )
                     {
-                    dispatch_apply( [ rectArray count ], defaultGlobalQueue
-                                  , ^( size_t _CurrentIndex )
-                                    {
-                                    [ self lockFocusIfCanDraw ];
-                                    [ NSGraphicsContext saveGraphicsState ];
+                    [ self lockFocusIfCanDraw ];
 
-                                    // The elements type for rectArray is NSValue, so we must invoke rectValue method to out-box the rectangle.
-                                    NSRect currentRect = [ [ rectArray objectAtIndex: _CurrentIndex ] rectValue ];
+                    [ NSGraphicsContext saveGraphicsState ];
+                    NSRect customClipRectOne = NSMakeRect( 50, 20, 100, 250 );
+                    NSRect customClipRectTwo = NSMakeRect( 500, 5, 100, 100 );
+                    NSRect customClipRectThree = NSMakeRect( 500, 150, 100, 100 );
+                    NSRect customClipRectFour = NSMakeRect( 250, 150, 70, 100 );
 
-                                    NSColor* currentColor = [ colorArray objectAtIndex: _CurrentIndex % [ colorArray count ] ];
+                    NSBezierPath* customClipRegionOne = [ NSBezierPath bezierPathWithRect: customClipRectOne ];
+                    [ customClipRegionOne appendBezierPathWithRect: customClipRectTwo ];
+                    [ customClipRegionOne appendBezierPathWithOvalInRect: customClipRectThree ];
+//                    [ customClipRegionOne appendBezierPathWithOvalInRect: customClipRectFour ];
 
-                                    [ currentColor set ];
-                                    NSRect tmpRect = NSInsetRect( currentRect, -( CGFloat )_CurrentIndex, -( CGFloat )_CurrentIndex );
+                    NSBezierPath* customClipRegionTwo = [ NSBezierPath bezierPathWithOvalInRect: customClipRectFour ];
 
-                                    NSFrameRectWithWidth( tmpRect, _CurrentIndex );
+                    [ customClipRegionOne addClip ];
+                    [ customClipRegionTwo setClip ];
 
-                                    [ [ NSGraphicsContext currentContext ] flushGraphics ];
+                    NSFrameRect( customClipRectOne );
+                    NSFrameRect( customClipRectTwo );
+                    NSFrameRect( customClipRectThree );
+//                    NSFrameRect( customClipRectFour );
 
-                                    [ NSGraphicsContext restoreGraphicsState ];
-                                    [ self unlockFocus ];
-                                    } );
+//                    NSRectClip( customClipRectTwo );
+//                    NSRectClip( customClipRectOne );
+
+                    NSColor* RGBColor = [ NSColor colorWithCalibratedRed: 124.f / 255.f
+                                                                green: 113.f / 255.f
+                                                                 blue: 183.f / 255.f
+                                                                alpha: 0.9 ];
+
+                    NSColor* CMYKColor = [ NSColor colorWithDeviceCyan: 0.6324f
+                                                               magenta: 0.1843f
+                                                                yellow: 0.1571f
+                                                                 black: 0.0221f
+                                                                 alpha: 1.f ];
+                    CGFloat rectWidth = 100.f;
+                    CGFloat rectHeight = 50.f;
+                    CGFloat widthGap = 20.f;
+                    CGFloat heightGap = 30.f;
+                    NSRect drawingArea = NSMakeRect( self.frame.origin.x + widthGap, self.frame.origin.y + heightGap
+                                                   , self.frame.size.width - self.frame.origin.x * 2
+                                                   , self.frame.size.height - self.frame.origin.y * 2 );
+
+
+                    NSRect lastRect = NSMakeRect( drawingArea.origin.x, drawingArea.origin.y, rectWidth, rectHeight );
+                    int fuckVal = 0;
+
+                    for ( size_t index = 0; index < 40; index ++ )
+                        {
+                        [ [ NSGraphicsContext currentContext ] saveGraphicsState ];
+
+                        NSRect originalRect = lastRect;
+                        originalRect.origin.x = ( lastRect.size.width + widthGap ) * fuckVal++ + widthGap;
+                        originalRect.origin.y = lastRect.origin.y;
+
+                        if ( ( originalRect.origin.x + originalRect.size.width ) > drawingArea.size.width )
+                            {
+                            int static transboundaryCount = 1;
+                            fuckVal = 0;
+                            originalRect.origin.x = drawingArea.origin.x;
+                            originalRect.origin.y = heightGap + ( heightGap + originalRect.size.height ) * transboundaryCount++;
+                            }
+
+                        NSBezierPath* roundedRectPath = [ NSBezierPath bezierPathWithRoundedRect: originalRect xRadius: index yRadius: index ];
+                        [ roundedRectPath setLineWidth: 10 ];
+                        [ CMYKColor set ];
+                        [ roundedRectPath stroke ];
+
+                        [ RGBColor set ];
+                        [ roundedRectPath fill ];
+
+                        lastRect = originalRect;
+
+                        [ [ NSGraphicsContext currentContext ] flushGraphics ];
+                        [ [ NSGraphicsContext currentContext ] restoreGraphicsState ];
+                        }
+
+                    [ NSGraphicsContext restoreGraphicsState ];
+                    [ self unlockFocus ];
                     } );
-#endif
-
-#if 0   // Serial drawing
-    for ( int _CurrentIndex = 0; _CurrentIndex < [ rectArray count ]; _CurrentIndex++ )
-        {
-        [ NSGraphicsContext saveGraphicsState ];
-
-        // The elements type for rectArray is NSValue, so we must invoke rectValue method to out-box the rectangle.
-        NSRect currentRect = [ [ rectArray objectAtIndex: _CurrentIndex ] rectValue ];
-
-        NSColor* currentColor = [ colorArray objectAtIndex: _CurrentIndex % [ colorArray count ] ];
-
-        [ currentColor set ];
-        NSRect tmpRect = NSInsetRect( currentRect, -( CGFloat )_CurrentIndex, -( CGFloat )_CurrentIndex );
-
-        NSFrameRectWithWidth( tmpRect, _CurrentIndex );
-
-        [ [ NSGraphicsContext currentContext ] flushGraphics ];
-
-        [ NSGraphicsContext restoreGraphicsState ];
-        }
-#endif
     }
 
 @end // LIContentView
